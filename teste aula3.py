3d663cb2fbed545dff147d4d043dc14077ccd3d4
JoaoSusin <joao.psusin@gmail.com>
2026-01-06 17:43:35 -0300

    Update teste aula3.py
    terminadno a aula de dicionario

--- teste aula3.py
+++ teste aula3.py
@@ -4,10 +4,18 @@
 #for k ,v in pessoas.items():
 #   print(f'{k} = {v}')
 
-brasil = []
-estado1 = {'uf': 'Rio de janeiro', 'sigla': 'RJ'}
-estado2 = {'uf': 'Rio Grande do Sul', 'sigla': 'RS'}
-brasil.append(estado1)
-brasil.append(estado2)
+#brasil = []
+#estado1 = {'uf': 'Rio de janeiro', 'sigla': 'RJ'}
+#estado2 = {'uf': 'Rio Grande do Sul', 'sigla': 'RS'}
+#brasil.append(estado1)
+#brasil.append(estado2)
 
-print(brasil[0]['uf'])
+#print(brasil[0]['uf'])
+
+estado = {}
+brasil = []
+for c in range (0,3):
+    estado['uf']= str(input('Unidade Federativa: '))
+    estado['Sigla'] = str(input('Sigla do estado: '))
+    brasil.append(estado.copy())
+print(brasil)
