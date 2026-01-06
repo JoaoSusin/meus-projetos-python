617ba6bb342f16a51f33ce45d063cab4f255f6d9
JoaoSusin <joao.psusin@gmail.com>
2026-01-06 17:35:36 -0300

    Create teste aula3.py
    aula de dicionario em python

---
+++ teste aula3.py
@@ -0,0 +1,13 @@
+#pessoas = {'nome':'Gustavo', 'sexo': 'Masculino', 'idade': 20}
+#pessoas['peso'] = 98.5
+#print(f'O {pessoas["nome"]} tem {pessoas["idade"]} anos')
+#for k ,v in pessoas.items():
+#   print(f'{k} = {v}')
+
+brasil = []
+estado1 = {'uf': 'Rio de janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'Rio Grande do Sul', 'sigla': 'RS'}
+brasil.append(estado1)
+brasil.append(estado2)
+
+print(brasil[0]['uf'])
