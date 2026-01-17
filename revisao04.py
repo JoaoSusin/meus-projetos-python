e76f376cd31086a55f1e5a07f8068481288e0588
JoaoSusin <joao.psusin@gmail.com>
2026-01-16 23:45:48 -0300

    auka

---
+++ revisao04.py
@@ -0,0 +1,13 @@
+#def titulo(txt):
+#    print('-'*30)
+#    print(txt)
+#    print('-'*30)
+#titulo('Mensagem!!')
+#titulo('Python')
+
+def soma(a,b):
+    s = a + b
+    print(f'A soma de {a} + {b} = {s}')
+
+soma(7,4)
+soma(6,3)
